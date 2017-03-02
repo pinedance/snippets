f184766bae5a09a5a541c8ea9bd64c9e0e9f5a40
Junho <pinedance@users.noreply.github.com>
2017-03-02 20:26:49 +0900

    Create downloadFiles.sh

---
+++ bash/downloadFiles.sh
@@ -0,0 +1,5 @@
+# axel : 가속다운로드
+# wget과 옵션은 유사하다.
+# sudo apt-get install axel
+
+axel --header="Accept: */*" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" http://contrast.ani.today/files/videos/1488429979d397c.mp4
