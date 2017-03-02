8df58a29a420f3ac6f355cc14d9fc59173f6f4e3
Junho <pinedance@users.noreply.github.com>
2017-03-02 20:18:52 +0900

    Create wget.sh

---
+++ bash/wget.sh
@@ -0,0 +1,3 @@
+# -c : 이어받기
+
+wget --header="Accept: */*" --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" -c http://contrast.ani.today/files/videos/1488429979d397c.mp4
