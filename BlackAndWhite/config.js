c0f85cf38b174d96acc94a22aac37f121c8b9d0d
kiomime <kiomime@kiom.re.kr>
2017-07-13 08:35:46 +0900

    update BlackAndWhite

--- BlackAndWhite/config.js
+++ BlackAndWhite/config.js
@@ -2,8 +2,8 @@ module.exports = {
     
     
     // imgPath: "http://kostma.korea.ac.kr/data/des/RIKS+CRMA+KSM-WZ.1852.0000-20170331.KY_W_332/IMG/KY_W_332_001/0003.jpg"
-    "sourcePath": "./sourceData",
-    "resultPath": "./resultData",
+    "sourcePath": "./_sourceData",
+    "resultPath": "./_resultData",
     "blackCutoff": 120,
     "backgroundColor": 0xFFFFFFFF,
     
--- BlackAndWhite/main.js
+++ BlackAndWhite/main.js
@@ -7,24 +7,57 @@ var conf = require("./config");
 var ctrl = require("./controller");
 
 
-// excute main
-var files = fs.readdirSync( conf.sourcePath ) 
-
-var cutoff = ( process.argv[2] )? Number( process.argv[2] ) : conf.blackCutoff
-
-console.log( "\n# Process is ready to start. (cutoff value: " + cutoff.toString()  + ")\n" )
-
-var now = new Date().toISOString()
-
-var _rstPath = [conf.resultPath, now + "_" + cutoff].join("/")
-
-fs.mkdirSync( _rstPath  )
-
-files.forEach(function(e, i, arr){
+function dircheck( dirPath, callback ) {
 	
-	var src_file = [conf.sourcePath, e].join("/")
-	var rst_file = [_rstPath, e].join("/")
+	fs.stat( dirPath, function (err, stats){
+		if (err) {  // Directory doesn't exist or something.
+	    	console.log('Folder doesn\'t exist, so I made the folder ' + dirPath );
+	    	return fs.mkdir(dirPath, callback);
+		}
+	  	
+	  	if (!stats.isDirectory()) { // This isn't a directory!
+	    	console.error( new Error('Folder doesn\'t exist, so I made the folder ' + dirPath) );
+	    	fs.mkdir(dirPath, callback);
+	  	} else {
+	    	callback()
+		}
+	  
+	});
 	
-	ctrl.makeBlackAndWhite( src_file, rst_file, cutoff, conf.backgroundColor )
-})
+}  // https://stackoverflow.com/questions/35849474/node-check-to-see-if-a-directory-exists
+
+
+dircheck( conf.sourcePath, function(){ 
+	
+	dircheck( conf.resultPath, function(){
+
+
+		// excute main
+		
+		var files = fs.readdirSync( conf.sourcePath )	
+		
+		var cutoff = ( process.argv[2] )? Number( process.argv[2] ) : conf.blackCutoff
+
+		console.log( "\n# Process is ready to start. (cutoff value: " + cutoff.toString()  + ")\n" )
+		
+		var now = new Date().toISOString()
+		
+		var _rstPath = [conf.resultPath, now + "_" + cutoff].join("/")
+		
+		fs.mkdirSync( _rstPath  )
+		
+		files.forEach(function(e, i, arr){
+			
+			var src_file = [conf.sourcePath, e].join("/")
+			var rst_file = [_rstPath, e].join("/")
+			
+			ctrl.makeBlackAndWhite( src_file, rst_file, cutoff, conf.backgroundColor )
+		})
+	
+		
+	});
+
+});
+
+
 
